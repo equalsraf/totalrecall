c43ed48df9ad0da9b694c46fd54e23b6008fe62d
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-05-05 14:12:58 +0100

    Cleanup: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ use std::thread::sleep;
 use std::time::Duration;
 
 #[cfg(unix)]
-use std::os::unix::process::ExitStatusExt ;
+use std::os::unix::process::ExitStatusExt;
 
 #[cfg(unix)]
-fn signal(status: &ExitStatus) -> Option<i32> { status.signal() }
+fn signal(status: &ExitStatus) -> Option<i32> {
+    status.signal()
+}
 #[cfg(not(unix))]
-fn signal(_: &ExitStatus) -> Option<i32> { None }
+fn signal(_: &ExitStatus) -> Option<i32> {
+    None
+}
 
 fn main() {
     let arguments: Vec<_> = env::args().collect();
@@ -24,34 +28,37 @@ fn main() {
     loop {
         let start = PreciseTime::now();
         match Command::new(&arguments[1])
-                .args(&arguments[2..])
-                .status() {
+                  .args(&arguments[2..])
+                  .status() {
             Err(err) => {
                 println!("Error running {:?}: {}", &arguments, err);
                 exit(-1);
-            },
+            }
             Ok(status) if status.success() => exit(0),
             Ok(status) => {
                 if let Some(signum) = signal(&status) {
-                    println!("totalrecall: {} exited with signal({}), exiting",
-                            arguments[1], signum);
+                    println!("totalrecall: \"{}\" exited with signal({}), exiting",
+                             arguments[1],
+                             signum);
                     exit(-1);
                 }
 
                 if let Some(code) = status.code() {
                     if let Ok(elapsed) = start.to(PreciseTime::now()).to_std() {
-		        if elapsed.as_secs() < 2 {
+                        if elapsed.as_secs() < 2 {
                             sleep(Duration::from_secs(1))
-		        }
-                        println!("totalrecall: After {}s {} exited with code({}), restarting",
-                                elapsed.as_secs(),
-                                arguments[1], code);
+                        }
+                        println!("totalrecall: After {}s \"{}\" exited with code({}), restarting",
+                                 elapsed.as_secs(),
+                                 arguments[1],
+                                 code);
                     } else {
-                        println!("totalrecall: {} exited with code({}), restarting",
-                                arguments[1], code);
+                        println!("totalrecall: \"{}\" exited with code({}), restarting",
+                                 arguments[1],
+                                 code);
                     }
                 } else {
-                    println!("totalrecall: {} terminated", arguments[1]);
+                    println!("totalrecall: \"{}\" terminated", arguments[1]);
                     exit(-1);
                 }
             }
